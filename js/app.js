6e8bae9d48286f4e8722bff0b197ac869d132736
Deceit-87 <manuelfreund318@gmail.com>
2022-02-28 19:25:42 +0100

    aggiunte immagini, reset html and css

---
+++ js/app.js
@@ -0,0 +1,50 @@
+const app = new Vue({
+
+        el:'#app',
+        data:{
+            
+            SLIDES =[
+                {
+                    imgUrl: 'img/01.jpg' , 
+                    titleImg:'Svezia',
+                    descriptionImg:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            
+            
+                },
+                {
+                    imgUrl: 'img/02.jpg' , 
+                    titleImg:'Svizzera',
+                    descriptionImg:'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            
+            
+                },
+                {
+                    imgUrl: 'img/03.jpg' , 
+                    titleImg:'Gran-bretagna',
+                    descriptionImg:'Lorem ipsum.',
+            
+            
+                },
+                {
+                    imgUrl: 'img/04.jpg' , 
+                    titleImg:'Germania',
+                    descriptionImg:'Lorem ipsum,cumque provident totam omnis.',
+            
+            
+                },
+                {
+                    imgUrl: 'img/05.jpg' , 
+                    titleImg:'Pardise',
+                    descriptionImg:'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            
+            
+                }
+            ]
+            
+        }
+
+})
+
+
+
+
